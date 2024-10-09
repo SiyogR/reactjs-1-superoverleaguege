08bc39b8478c83f688e58c05808a26239a7a5e7e
SiyogR <rsiyog108@gmail.com>
2024-10-09 17:03:39 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className='bg-container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='img'>
+      <img
+        src='https://assets.ccbp.in/frontend/content/react-js/super-over-league-lg-output.png'
+        className='text-center'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
